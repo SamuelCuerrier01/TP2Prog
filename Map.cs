664e041d53fa52624e36fd2f6b617955a8d5bd56
Samuel Cuerrier <6297553@cstjean.qc.ca>
2025-09-23 11:02:37 -0400

    début de la classe map

--- Map.cs
+++ Map.cs
@@ -2,5 +2,15 @@
 
 public class Map
 {
-    
+    private Parc _parc;
+
+    public Map(Parc parc)
+    {
+        _parc = parc;
+    }
+
+    public void afficherParc()
+    {
+        Console.WriteLine(_parc);
+    }
 }
